b192dd6451326571688a3ab2957e20f51dc29f94
Sultan <Sul975536@gmail.com>
2026-01-21 22:03:58 +0300

    added XOR based func cryption

---
+++ crypt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void xorcrypt(FILE*,FILE*,const char*,int);
+int main(int argc,char *argv[]){
+    if(argc != 4){
+        printf("Error : enter 3 parameter (infile , outfile , key)");
+        return 1;
+    }
+    FILE* inputfile = fopen(argv[1],"rb");
+    FILE* outputfile = fopen(argv[2],"wb");
+    if(!inputfile || !outputfile){
+        printf("Error opening files.");
+        return 1;
+    }
+    xorcrypt(inputfile,outputfile,argv[3],strlen(argv[3]));
+    printf("encryption succesful!");
+    return 0;
+}
+
+void xorcrypt(FILE* in,FILE* out,const char* key,int keylen){
+    int byte; int cnt = 0;
+    while(1){
+        byte = fgetc(in);
+        if(byte == EOF) return;
+        unsigned char crypt = byte ^ key[cnt++ % keylen];
+        fputc(crypt,out);
+    }
+}
